c72f145c5a31fed6e84201a77c8d9f4252cbf90d
Daniel Rossa <daniel@rossa-fam.com>
2021-02-03 18:56:07 +0100

    This is the initial commit

---
+++ Powerbar_CustomText.lua
@@ -0,0 +1,11 @@
+function(total,value)
+    
+    local percent = tonumber(string.format("%.0f", value / total * 100));
+
+    if not aura_env.config.defaultColor then
+        aura_env.region:Color(aura_env.ressourceColor(UnitPowerType("player")));
+    end
+    
+    return percent;
+    
+end
