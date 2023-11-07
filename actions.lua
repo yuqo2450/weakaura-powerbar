43275d0702aea544d0328375f4baa7820d1a8277
Daniel Rossa <79540477+yuqo2450@users.noreply.github.com>
2023-11-07 17:47:28 +0100

    Update for game version 10.2.0 (#2)
    * changed: refactored code and directory structure
    
    * added: action to auto create release
    
    * added: changelog
    
    * updated: import string for version 10.2.0

--- actions.lua
+++ actions.lua
@@ -1,52 +1,27 @@
 function aura_env.ressourceColor (powerType,powerToken,altR,altG,altB)
-    
     local color;
     local powerColor = aura_env.config;
-    
-    
-    if powerToken == "MANA" then
-        
-        color = powerColor.mana;
-        
-    elseif powerToken == "RAGE" then
-        
-        color = powerColor.rage;
-        
-    elseif powerToken == "FOCUS" then
-        
-        color = powerColor.focus;
-        
-    elseif powerToken == "ENERGY" then
-        
-        color = powerColor.energy;
-        
-    elseif powerToken == "RUNIC_POWER" then
-        
-        color = powerColor.runicPower;
-        
-    elseif powerToken == "LUNAR_POWER" then
-        
-        color = powerColor.lunarPower;        
-        
-    elseif powerToken == "MAELSTROM" then
-        
-        color = powerColor.maelstrom;
-        
-    elseif powerToken == "FURY" then
-        
-        color = powerColor.fury;
-        
-    elseif powerToken == "PAIN" then
-        
-        color = powerColor.pain;
-        
-    elseif powerToken == "INSANITY" then
-        
-        color = powerColor.insanity;
-        
-    end
-    
-    return color[1], color[2], color[3], color[4];
-    
-end
 
+    if powerToken == "MANA" then
+        color = powerColor.mana;
+    elseif powerToken == "RAGE" then
+        color = powerColor.rage;
+    elseif powerToken == "FOCUS" then
+        color = powerColor.focus;
+    elseif powerToken == "ENERGY" then
+        color = powerColor.energy;
+    elseif powerToken == "RUNIC_POWER" then
+        color = powerColor.runicPower;
+    elseif powerToken == "LUNAR_POWER" then
+        color = powerColor.lunarPower;
+    elseif powerToken == "MAELSTROM" then
+        color = powerColor.maelstrom;
+    elseif powerToken == "FURY" then
+        color = powerColor.fury;
+    elseif powerToken == "PAIN" then
+        color = powerColor.pain;
+    elseif powerToken == "INSANITY" then
+        color = powerColor.insanity;
+    end
+    return color[1], color[2], color[3], color[4];
+end
